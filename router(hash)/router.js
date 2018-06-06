c7b52f764f76c28e7991c0292a7050368de9470f
jx915 <jx915zz@163.com>
2018-06-06 15:00:58 +0800

    前端路由实现

--- router(hash)/router.js
+++ router(hash)/router.js
@@ -1,3 +1,4 @@
+//定义路由
 function Router(){
     this.routes={};
     this.currentURL='';
@@ -16,24 +17,25 @@ Router.prototype.init = function () {
     window.addEventListener('load',this.refresh.bind(this),false);
     window.addEventListener('hashchange',this.refresh.bind(this),false);
 }
- 
+
+// 地址栏hash值改变
 function display_page(url){
  
-    $("#router").load('./'+ url +'.html')
+    $("#router").load(url)
 }
  
 window.Router = new Router();
  
 Router.route('/index',function(){
-    display_page('main');
+    display_page('./main.html');
 })
  
 Router.route('/news',function(){
-    display_page('news');
+    display_page('./news.html');
 })
  
 Router.route('/about',function(){
-    display_page('about');
+    display_page('./about.html');
 })
  
 window.Router.init();
